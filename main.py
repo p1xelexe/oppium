021749a2967f30009cfcd057dfa0f264e671d7b1
p1xelexe <123638554+p1xelexe@users.noreply.github.com>
2024-05-15 13:51:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,123 +3,57 @@ from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
 
-capabilities = dict(
-    platformName='Android',
-    automationName='uiautomator2',
-    deviceName='Android',
-    language='en',
-    locale='US'
-)
+capabilities = {
+    'platformName': 'Android',
+    'automationName': 'uiautomator2',
+    'deviceName': 'Android',
+    'language': 'en',
+    'locale': 'US'
+}
 
 appium_server_url = 'http://localhost:4723'
 
 class TestAppium(unittest.TestCase):
-    def setUp(self) -> None:
-        self.driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
 
-    def tearDown(self) -> None:
-        if self.driver:
-            self.driver.quit()
+    @classmethod
+    def tearDownClass(cls) -> None:
+        if cls.driver:
+            cls.driver.quit()
+
+    def click_button(self, by, value) -> None:
+        el = self.driver.find_element(by=by, value=value)
+        el.click()
+
+    def delete_all(self) -> None:
+        for _ in range(3):
+            self.click_button(AppiumBy.XPATH, '//android.widget.ImageButton[@content-desc="delete"]')
 
     def test_plus(self) -> None:
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.Button[@content-desc="plus"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.ImageButton[@content-desc="delete"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.ImageButton[@content-desc="delete"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.ImageButton[@content-desc="delete"]')
-        el.click()
+        self.click_button(AppiumBy.XPATH, '//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
+        self.click_button(AppiumBy.XPATH, '//android.widget.Button[@content-desc="plus"]')
+        self.click_button(AppiumBy.XPATH, '//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
+        self.delete_all()
 
     def test_minus(self) -> None:
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.Button[@content-desc="minus"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.ImageButton[@content-desc="delete"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.ImageButton[@content-desc="delete"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.ImageButton[@content-desc="delete"]')
-        el.click()
+        self.click_button(AppiumBy.XPATH, '//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
+        self.click_button(AppiumBy.XPATH, '//android.widget.Button[@content-desc="minus"]')
+        self.click_button(AppiumBy.XPATH, '//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
+        self.delete_all()
 
     def test_multiplication(self) -> None:
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.Button[@content-desc="×"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.ImageButton[@content-desc="delete"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.ImageButton[@content-desc="delete"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.ImageButton[@content-desc="delete"]')
-        el.click()
+        self.click_button(AppiumBy.XPATH, '//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
+        self.click_button(AppiumBy.XPATH, '//android.widget.Button[@content-desc="×"]')
+        self.click_button(AppiumBy.XPATH, '//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
+        self.delete_all()
 
     def test_divide(self) -> None:
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.Button[@content-desc="divide"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.ImageButton[@content-desc="delete"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.ImageButton[@content-desc="delete"]')
-        el.click()
-
-        el = self.driver.find_element(by=AppiumBy.XPATH,
-                                      value='//android.widget.ImageButton[@content-desc="delete"]')
-        el.click()
+        self.click_button(AppiumBy.XPATH, '//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
+        self.click_button(AppiumBy.XPATH, '//android.widget.Button[@content-desc="divide"]')
+        self.click_button(AppiumBy.XPATH, '//android.widget.Button[@resource-id="com.google.android.calculator:id/digit_4"]')
+        self.delete_all()
 
 if __name__ == '__main__':
     unittest.main()
